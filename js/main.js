40912e9e5e3e100cd8b2cab2db9d6afdcef41fb3
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-10-04 13:22:17 +0300

    V2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-import html2canvas from 'html2canvas'
-
+import './html2canvas.min.js'
 document.addEventListener('DOMContentLoaded', () => {
     const currentUrl = window.location.href
     const barcodeImg = document.querySelector('.body-barcode-image img')
@@ -9,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         barcodeImg.src = `https://barcode.tec-it.com/barcode.ashx?code=EAN13&data=${urlStr}`
         html2canvas(document.querySelector('.result-barcode-image'))
             .then(canvas => {
-                document.body.appendChild(canvas)
+                document.querySelector('meta[property="og:image"]').setAttribute("content", canvas.toDataURL());
             })
     }
 })
